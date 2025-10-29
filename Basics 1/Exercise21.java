c68fe3fcf8e3a01ad935b4bbd0d7768400d349cf
Md Akram <akrammd013@gmail.com>
2025-10-29 20:49:21 +0530

    22 added

---
+++ Basics 1/Exercise21.java
@@ -0,0 +1,36 @@
+// Write a Java program to convert a binary number to a decimal number.
+
+import java.util.Scanner;
+
+public class Exercise21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input a binary number: ");
+        int bin = sc.nextInt();
+
+        int decimal = bin_to_deci(bin);
+        if (decimal != -1) {
+            System.out.println("Decimal Number: " + decimal);
+        }
+    }
+
+    public static int bin_to_deci(int bin) {
+        int deci = 0;
+        int exponent = 0;
+
+        while (bin > 0) {
+            int rem = bin % 10;
+
+            if (rem > 1) {
+                System.out.println("Not a binary number!!");
+                return -1;
+            }
+
+            deci += rem * (1 << exponent);  // same as 2^exponent
+            exponent++;
+            bin /= 10;
+        }
+        return deci;
+    }
+}
