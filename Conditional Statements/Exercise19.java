e031c346765df3eda8c9697f8cd928a2ce2a1f6e
Md Akram <akrammd013@gmail.com>
2025-11-21 23:46:00 +0530

    file added

--- Conditional Statements/Exercise19.java
+++ Conditional Statements/Exercise19.java
@@ -13,5 +13,18 @@ public class Exercise19 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows:  ");
+        int rows = sc.nextInt();
+        sc.close();
+        
+        for(int i=1; i<=rows; i++){
+            for(int j=1;j<=rows-i; j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
     }
 }
