bb86c350b3c940a4b5b5f77016d81b21883b0aed
Md Akram <akrammd013@gmail.com>
2025-11-23 23:50:18 +0530

    added in cond.

---
+++ Conditional Statements/Exercise22.java
@@ -0,0 +1,48 @@
+/*
+22. Pascal's Triangle
+Write a Java program to display Pascal's triangle.
+Test Data
+Input number of rows: 5
+Expected Output :
+
+Input number of rows: 5                                                          
+      1                                                                          
+     1 1                                                                         
+    1 2 1                                                                        
+   1 3 3 1                                                                       
+  1 4 6 4 1        
+*/
+
+public class Exercise22 {
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
